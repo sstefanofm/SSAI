5b8a3db588334fd7cac14acb529c207bebf110bc
stf <sstefanofm@gmail.com>
2024-01-24 19:17:29 -0300

    Provide token like a BOSS.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-
 import './App.css'
+import TokenProvider from './context/TokenProvider'
 import Artists from './components/Artists'
 
-const getToken = async () => await (await fetch('http://localhost:8080')).json()
-
 function App() {
-  const [token, setToken] = useState('')
-
   return (
-    <>
-      <button onClick={async () => setToken((await getToken()).access_token)}>get token</button>
-      <p>{token}</p>
-
-      {token && <Artists token={token} />}
-    </>
+    <TokenProvider>
+      <Artists />
+    </TokenProvider>
   )
 }
 
