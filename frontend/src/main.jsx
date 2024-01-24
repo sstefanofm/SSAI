e6170bfdfb45cf7fb1fd4329c2721c7eb5a93083
stf <sstefanofm@gmail.com>
2024-01-24 19:41:08 -0300

    Implemented routes with React Router and useRoutes <3.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,8 @@
+import Artists from './components/Artists'
+
+export const routes = [
+  {
+    path: '/artists',
+    element: <Artists />,
+  },
+]
