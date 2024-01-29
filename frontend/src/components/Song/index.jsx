3a48eefb2be40c5fa9c535f90fee3484d931852e
stf <sstefanofm@gmail.com>
2024-01-29 12:58:38 -0300

    Songs have duration in unformatted milliseconds.

--- frontend/src/components/Song/index.jsx
+++ frontend/src/components/Song/index.jsx
@@ -1,11 +1,12 @@
 const Song = ({ song, fromAlbum = false, order = -1 }) => {
   return (
     <li>
-      {fromAlbum && <span>{order}</span>}
+      {fromAlbum && <div>{order}</div>}
       {!fromAlbum && <img src={song.album.images[1].url} alt={`${song.album.name} album cover`} />}
       <h3>{song.name}</h3>
       <h4>{song.artists[0].name}</h4>
-      <div>{song.explicit && 'explicit'}</div>
+      {song.explicit && <div>explicit</div>}
+      <div>{song.duration_ms}</div>
     </li>
   )
 }
