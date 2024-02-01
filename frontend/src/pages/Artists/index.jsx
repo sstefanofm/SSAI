639d2d799754313c40a5eadb0a82f90ff495e670
stf <sstefanofm@gmail.com>
2024-02-01 20:54:41 -0300

    Rendering artists with a List in Artists page.

--- frontend/src/pages/Artists/index.jsx
+++ frontend/src/pages/Artists/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import './Artists.css'
 import ArtistService from '../../services/ArtistService'
 import { TokenContext } from '../../context/TokenProvider'
+import List from '../../components/List'
 
 const Artists = () => {
   const token = useContext(TokenContext)
@@ -23,19 +24,7 @@ const Artists = () => {
   return (
     <>
       <h2>Artists</h2>
-      <div className='Artists'>
-        {artists.map(a => {
-          return (
-            <Link key={a.id} to={a.id} className='Artist'>
-              <img className='Artist__Image' src={a.images[1].url} />
-              <div className="Artist__Data">
-                <h3 className='Artist__Name'>{a.name}</h3>
-                <span>{a.followers.total}</span>
-              </div>
-            </Link>
-          )
-        })}
-      </div>
+      <List elements={artists} />
     </>
   )
 }
