11095a9b0681e4a0b5a0a787fe34cd5e56d665d0
stf <sstefanofm@gmail.com>
2024-02-01 19:47:19 -0300

    Styled track player.

--- frontend/src/components/Track/index.jsx
+++ frontend/src/components/Track/index.jsx
@@ -1,13 +1,20 @@
 import { useContext } from 'react'
 import ReactAudioPlayer from 'react-audio-player'
+
+import './Track.css'
 import { TrackContext } from '../../context/TrackContext'
 
 const Track = () => {
   const { track } = useContext(TrackContext)
 
   return (
-    <div>
-      <ReactAudioPlayer src={track.preview_url} autoPlay={true} controls={true} />
+    <div className='Track'>
+      <ReactAudioPlayer
+        className='Track__Player'
+        src={track.preview_url}
+        autoPlay={true}
+        controls={true}
+      />
     </div>
   )
 }
