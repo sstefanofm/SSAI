80f2c65a052d249f683388acf2639f799b8931de
stf <sstefanofm@gmail.com>
2024-02-01 20:49:21 -0300

    Basic styles to Artists list.

--- frontend/src/components/Link/index.jsx
+++ frontend/src/components/Link/index.jsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react'
 import { Link as LinkRR } from 'react-router-dom'
 
+import './Artist.css'
 import { IconCover, IconPerson } from '../Icon'
 import { TrackContext } from '../../context/TrackContext'
 
-const renderCover = element => {
+const renderCover = (element, name) => {
   if (element.type === 'track') element = element.album
 
   /* is track from Album page (no need to show cover) */
@@ -17,17 +18,15 @@ const renderCover = element => {
     return <IconCover size={300} />
   }
 
-  return <img src={element.images[0].url} />
+  return <img className={`${name}__Image`} src={element.images[0].url} />
 }
-
-// https://p.scdn.co/mp3-preview/01c723bc6697303c1f402c9d15d0e96b7eb6fbee?cid=c2499296b6e342ac82ae74465ba02356
 
 const renderAdditionalElements = (element, playTrack) => {
   switch (element.type) {
     case 'album':
       return <h4>{new Date(element.release_date).getFullYear()}</h4>
     case 'artist':
-      return <h4>{element.followers.total}</h4>
+      return <h4>{element.followers.total} followers</h4>
     case 'track':
       return (
         <>
@@ -52,12 +51,17 @@ const renderAdditionalElements = (element, playTrack) => {
 const Link = ({ element }) => {
   const { setTrack } = useContext(TrackContext)
 
+  const type = element.type
+  const capitalizedType = type.charAt(0).toUpperCase() + type.slice(1)
+
   return (
-    <LinkRR to={`/${element.type}s/${element.id}`}>
+    <LinkRR className={capitalizedType} to={`/${element.type}s/${element.id}`}>
       {!element.album && <div>{element.track_number}</div>}
-      {renderCover(element)}
-      <h4>{element.name}</h4>
-      {renderAdditionalElements(element, setTrack)}
+      {renderCover(element, capitalizedType)}
+      <div className={`${capitalizedType}__Data`}>
+        <h4>{element.name}</h4>
+        {renderAdditionalElements(element, setTrack)}
+      </div>
     </LinkRR>
   )
 }
