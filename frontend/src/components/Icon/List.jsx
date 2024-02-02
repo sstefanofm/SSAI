337dfc184d1828ad43485c4ac35b3681da5e5181
stf <sstefanofm@gmail.com>
2024-02-01 23:45:42 -0300

    BurgerMenu (poorly styled).

---
+++ frontend/src/components/Icon/List.jsx
@@ -0,0 +1,10 @@
+const List = ({ size = 30 }) => (
+  <svg width={size} height={size} fill='currentColor' viewBox='0 0 16 16'>
+    <path
+      fill-rule='evenodd'
+      d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5'
+    />
+  </svg>
+)
+
+export default List
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,26 +1,35 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Navbar.css'
 import viteLogo from '../../assets/vite.svg'
+import { IconList, IconBurger } from '../Icon'
 
 const Navbar = () => {
+  const [hidden, setHidden] = useState(false)
+
   return (
     <div className='Navbar'>
       <div className='Logo'>
         <img src={viteLogo} />
         <h1>SSAI</h1>
       </div>
-      <nav className='Navbar__Links'>
-        <Link to='/albums'>
-          <button>Albums</button>
-        </Link>
-        <Link to='/artists'>
-          <button>Artists</button>
-        </Link>
-        <Link to='/tracks'>
-          <button>Tracks</button>
-        </Link>
-      </nav>
+      <div>
+        <button className='Navbar__BurgerButton' onClick={() => setHidden(!hidden)}>
+          {hidden ? <IconList size={30} /> : <IconBurger size={30} />}
+        </button>
+        <nav hidden={hidden} className='Navbar__Links'>
+          <Link to='/albums'>
+            <button>Albums</button>
+          </Link>
+          <Link to='/artists'>
+            <button>Artists</button>
+          </Link>
+          <Link to='/tracks'>
+            <button>Tracks</button>
+          </Link>
+        </nav>
+      </div>
     </div>
   )
 }
