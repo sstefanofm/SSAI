94498fbd822f748cf29f10f5bff4852141a20a97
stf <sstefanofm@gmail.com>
2024-02-01 22:32:55 -0300

    Styled album view.

--- frontend/src/pages/Album/index.jsx
+++ frontend/src/pages/Album/index.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
+import './Album.css'
 import AlbumService from '../../services/AlbumService'
 import { TokenContext } from '../../context/TokenProvider'
 import List from '../../components/List'
@@ -20,8 +21,14 @@ const Album = () => {
   return (
     album && (
       <div>
-        <h2>{album.name}</h2>
-        <img src={album.images[0].url} alt={`${album.name} album cover`} />
+        <div className='Album__Data'>
+          <h2>{album.name}</h2>
+          <img
+            className='Album__Image'
+            src={album.images[0].url}
+            alt={`${album.name} album cover`}
+          />
+        </div>
         <List elements={album.tracks.items} />
       </div>
     )
