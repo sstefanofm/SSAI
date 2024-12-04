12c7a87c627fbae1a7c0b92a7cf6d313f8db5532
stf <sstefanofm@gmail.com>
2024-12-03 21:26:00 -0300

    dinamically set search placeholder

--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import './Search.css'
@@ -6,10 +6,17 @@ import { IconDrop, IconSearch } from '../Icon'
 import { TokenContext } from '../../context/TokenProvider'
 import SearchService from '../../services/SearchService'
 
+const searchTypeOptions = [
+  { value: 'album', label: 'Albums' },
+  { value: 'artist', label: 'Artists' },
+  { value: 'track', label: 'Tracks' }
+]
+
 const Search = () => {
   const token = useContext(TokenContext)
   const [searchString, setSearchString] = useState('')
-  const [searchType, setSearchType] = useState('album')
+  const [searchType, setSearchType] = useState(searchTypeOptions[0].value)
+  const [searchPlaceholder, setSearchPlaceholder] = useState('Search for...')
   const navigate = useNavigate()
 
   const performSearch = () => {
@@ -20,11 +27,27 @@ const Search = () => {
       .catch(console.warn)
   }
 
+  useEffect(() => {
+    const article = (() => {
+      switch (searchType) {
+        case searchTypeOptions[0].value:
+        case searchTypeOptions[1].value:
+          return 'an'
+        default:
+          return 'a'
+      }
+    })()
+
+    setSearchPlaceholder(
+      `Search for ${article} ${searchType}`
+    )
+  }, [searchType])
+
   return (
     <div className='Search'>
       <input
         type='text'
-        placeholder='What do you want to search for?'
+        placeholder={searchPlaceholder}
         name='search'
         onChange={e => setSearchString(e.target.value)}
         onKeyDown={e => {
