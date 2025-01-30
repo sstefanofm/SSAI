99d4c86abdc0b13b42db1737f1e48304b08ac75f
stf <sstefanofm@gmail.com>
2025-01-29 21:16:05 -0300

    in search page, show title along with searched string

--- frontend/src/pages/Search/index.jsx
+++ frontend/src/pages/Search/index.jsx
@@ -2,6 +2,8 @@ import { useLocation } from 'react-router-dom'
 
 import List from '../../components/List'
 import MainTitle from '../../components/MainTitle'
+import { capitalize } from '../../util/capitalize'
+import { getUrlQueryParamValue } from '../../util/getUrlQueryParamValue'
 
 const Search = () => {
   const { state } = useLocation()
@@ -9,12 +11,15 @@ const Search = () => {
   const elementType = Object.keys(state)[0]
 
   let title = Object.keys(state)[0]
-  title = title.charAt(0).toUpperCase() + title.slice(1)
+  let searchString = getUrlQueryParamValue(
+    state[title].href,
+    'query'
+  )
 
   return (
     <div>
-      <MainTitle>Search</MainTitle>
-      <h3>{title}</h3>
+      <MainTitle>Search for {title} with &quot;{searchString}&quot;</MainTitle>
+      <h3>{capitalize(title)}</h3>
       <List elements={state[elementType].items} />
     </div>
   )
