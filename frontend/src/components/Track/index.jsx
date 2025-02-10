d0a6e3e4fd1b8fbae1986775b7943e7b3ca92481
stf <sstefanofm@gmail.com>
2025-02-10 19:11:30 -0300

    playPause button initial skeleton

--- frontend/src/components/Track/index.jsx
+++ frontend/src/components/Track/index.jsx
@@ -1,8 +1,18 @@
+import './Track.css'
+import { IconPause, IconPlay } from '../Icon'
+
 const Track = ({ trackElement }) => {
 
   return trackElement && (
     <div className='Track'>
-      <div>
+      <div className='Track__Header'>
+        <div className="Track__PlayPauseButton">
+        {
+          false /* TODO */ ?
+          <IconPause size={20} /> :
+          <IconPlay size={20} />
+        }
+        </div>
         <img src={trackElement.album.images[2].url} />
       </div>
     </div>
