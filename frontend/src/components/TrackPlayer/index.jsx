efa08704061f33135129843e22a2fe53cfa039dd
stf <sstefanofm@gmail.com>
2025-02-27 20:45:43 -0300

    albums with no cover show text of noCover :c

--- frontend/src/components/TrackPlayer/index.jsx
+++ frontend/src/components/TrackPlayer/index.jsx
@@ -2,10 +2,11 @@ import { useContext } from 'react'
 import ReactAudioPlayer from 'react-audio-player'
 
 import './TrackPlayer.css'
-import { TrackPlayerContext } from '../../context/TrackPlayerContext'
+import { TrackContext } from '../../context/TrackContext'
 
 const TrackPlayer = () => {
-  const { track } = useContext(TrackPlayerContext)
+  //const { track } = useContext(TrackContext)
+  const track = {preview_url:'https://api.spotify.com/v1/tracks/20ZP3Om9DvDazXrNUC7WM2'}
 
   return (
     <div className='TrackPlayer'>
