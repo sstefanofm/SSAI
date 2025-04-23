58e7e7d81f69cc081326b8986bedbdc7e1199397
stf <sstefanofm@gmail.com>
2025-04-23 17:40:37 -0300

    track onClick play button catched on TrackPlayer with context

--- frontend/src/components/TrackPlayer/index.jsx
+++ frontend/src/components/TrackPlayer/index.jsx
@@ -1,18 +1,18 @@
-import { useContext } from 'react'
 import ReactAudioPlayer from 'react-audio-player'
 
 import './TrackPlayer.css'
-import { TrackContext } from '../../context/TrackContext'
+import { useTrack } from '../../context/TrackProvider'
 
 const TrackPlayer = () => {
-  //const { track } = useContext(TrackContext)
-  const track = {preview_url:'https://api.spotify.com/v1/tracks/20ZP3Om9DvDazXrNUC7WM2'}
+  const { currentTrack } = useTrack()
+
+  console.log(currentTrack)
 
   return (
     <div className='TrackPlayer'>
       <ReactAudioPlayer
         className='TrackPlayer__Player'
-        src={track.preview_url}
+        src={''}
         autoPlay={true}
         controls={true}
       />
